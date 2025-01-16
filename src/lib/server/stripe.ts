d05f4b2c8396ba3f484134c7ed3c474ef58c8816
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2025-01-15 22:59:29 -0800

    fix invalid import again

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_STRIPE_API_KEY } from '$env/static/private';
+import { PUBLIC_STRIPE_API_KEY } from '$env/static/public';
 import { Stripe } from 'stripe';
 
 export const stripe = new Stripe(PUBLIC_STRIPE_API_KEY);
