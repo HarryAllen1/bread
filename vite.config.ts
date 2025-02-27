7c31f5ca650d1ee969f5d0997a0931aa92352756
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-02-27 10:30:08 -0800

    reduce bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(),enhancedImages(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		enhancedImages(),
+		{
+			...threeMinifier(),
+			enforce: 'pre',
+		},
+		sveltekit(),
+	],
 	build: {
 		target: 'es2020',
 		sourcemap: true,
